e8bdf266ac06fb13395dd0814e457f8645cd0ebd
Luc <luc@lucemans.nl>
2025-02-05 22:59:37 +0100

    Introduce basic config

---
+++ app/src/config.rs
@@ -0,0 +1,34 @@
+use anyhow::Result;
+use figment::{providers::{Format, Toml}, Figment};
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    pub homeassistant: HomeAssistantConfig,
+    pub device: DeviceConfig,
+    pub display: DisplayConfig,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct HomeAssistantConfig {
+    pub mqtt_url: String,
+    pub mqtt_username: Option<String>,
+    pub mqtt_password: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct DeviceConfig {
+    pub name: String,
+    pub id: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct DisplayConfig {
+    pub sleep_time: Option<u32>,
+}
+
+pub fn load_config() -> Result<Config> {
+    let figment = Figment::new().merge(Toml::file("config.toml"));
+    let config = figment.extract::<Config>()?;
+    Ok(config)
+}
--- app/src/main.rs
+++ app/src/main.rs
@@ -1,8 +1,14 @@
 use anyhow::Result;
 
+pub mod config;
+
 #[async_std::main]
 async fn main() -> Result<()> {
     println!("Hello, world!");
+
+    let config = config::load_config()?;
+
+    println!("Config: {:?}", config);
 
     Ok(())
 }
